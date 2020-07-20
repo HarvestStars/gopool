80320a774e4a30e38803b390762a5d426f53b6df
dell <574817827@qq.com>
2020-07-20 11:32:35 +0800

    add time out for conn pool

--- db/mysql.go
+++ db/mysql.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/HarvestStars/gopool/protocol"
 	"github.com/jinzhu/gorm"
@@ -13,11 +14,12 @@ var DataBase *gorm.DB
 
 // Setup 启动mysql配置
 func Setup(user string, pwd string, host string, db string) {
-	url := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pwd, host, db)
+	url := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local&timeout=10s", user, pwd, host, db)
 	var err error
 	DataBase, err = gorm.Open("mysql", url)
 	if err != nil {
 		panic("failed to connect database")
 	}
+	DataBase.DB().SetConnMaxLifetime(2 * 3600 * time.Second) // 2小时空闲链接超时
 	DataBase.AutoMigrate(&protocol.MinerInfo{}, &protocol.BlockMined{}, &protocol.LiquidInfo{}, &protocol.LiquidHeight{})
 }
